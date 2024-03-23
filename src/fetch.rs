9d2353c7733c5f072caf48f13648bc284c7c43b3
Guillaume Leroi <guillaume@leroi.re>
2024-03-23 07:02:37 +0100

    feat(fetch): better error message

--- src/fetch.rs
+++ src/fetch.rs
@@ -1,12 +1,17 @@
-use anyhow::Error;
+use anyhow::{Context, Error};
 use bytes::Buf;
 use feed_rs::model::Feed;
 use reqwest::ClientBuilder;
 
 pub async fn url(url: &str) -> Result<Feed, Error> {
     let agent = ClientBuilder::new().build()?;
-    let resp = agent.get(url).send().await?;
+    let resp = agent
+        .get(url)
+        .send()
+        .await
+        .with_context(|| format!("could not fetch {}", url))?;
     let content = resp.bytes().await?;
-    let feed = feed_rs::parser::parse(content.reader())?;
+    let feed = feed_rs::parser::parse(content.reader())
+        .with_context(|| format!("could not parse {}", url))?;
     Ok(feed)
 }
